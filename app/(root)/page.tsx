8733e51d036f6176ece9fdfe9d1ccdd78961ded2
mehmet ali <beyzadetoprak@gmail.com>
2024-08-08 11:17:28 +0200

    charts implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Mehmet" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your accounts and transactions efficiently."
+          />
+        </header>
+        <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.30} />
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
