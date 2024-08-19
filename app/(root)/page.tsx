b66ef3fdae0017ac4528313c9deeb4fe7b76315e
mehmet ali <beyzadetoprak@gmail.com>
2024-08-19 16:43:10 +0200

    right sidebar and bank accounts implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Mehmet" };
+  const loggedIn = {
+    firstName: "Mehmet",
+    lastName: "TT",
+    email: "example@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -12,11 +17,23 @@ const Home = () => {
             type="greeting"
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
-            subtext="Access and manage your accounts and transactions efficiently."
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
-        <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.30} />
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
