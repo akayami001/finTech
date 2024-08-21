17288c6bbae3cbe9e294adaa6daf2f302791ba5f
mehmet ali <beyzadetoprak@gmail.com>
2024-08-21 09:22:24 +0200

    implement auth appwrite and sentry

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SigUp = () => {
-  return <div>SigUp</div>;
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
 
-export default SigUp;
+export default SignUp;
